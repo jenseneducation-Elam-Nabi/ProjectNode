8b0361e7f711a48a805107ed793c917cdc4884f8
jenseneducation-elam-nabi <elam@nabi.nu>
2019-12-08 18:29:24 +0100

    Made some major changes
    Made some major changes on all of the pages

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ const form = document.querySelector("form");
 
 const inputs = document.querySelectorAll("input, textarea");
 const errorMessages = document.querySelectorAll(".error");
-const required = ["firstname", "lastname", "phone", "email", "comment"];
+const required = ["firstname", "lastname", "phone", "email", "comment", "password", "password-confirm"];
 form.addEventListener("submit", validateForm);
 
 function validateForm(e) {
@@ -21,14 +21,14 @@ function validateForm(e) {
                 addError(input, "is required", fieldName);
                 error = true;
             } else if (fieldName == "firstname" || fieldName == "lastname") {
-                let checkForNumbers = /^[0-9]*$/;
+                let checkForNumbers = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/;
                 let result = checkForNumbers.test(input.value);
-                if (result) {
+                if (!result) {
                     addError(input, "can't contain any numbers", fieldName);
                     error = true;
                 }
             } else if (fieldName == "phone") {
-                let checkForNumbers = /^[0-9]*$/;
+                let checkForNumbers = /^[\+\-]?\d*\.?\d+(?:[Ee][\+\-]?\d+)?$/;
                 let result = checkForNumbers.test(input.value);
                 if (!result) {
                     addError(input, "number is not valid", fieldName);
@@ -38,19 +38,44 @@ function validateForm(e) {
                     error = true;
                 }
             } else if (fieldName == "email") {
-                let checkValidEmail = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+                let checkValidEmail = /([A-Za-z0-9._-]+@[A-Za-z0-9._-]+\.[A-Za-z0-9]+)\w+/;
                 let result = checkValidEmail.test(input.value);
                 if (!result) {
                     addError(input, "provided is not a valid e-mail address", fieldName);
                     error = true;
                 }
             }
+            if (fieldName == "password") {
+                let exp = /[A-Za-z0-9]+$/;
+                let result = exp.test(input.value);
+                if (!result) {
+                    addError(input, "Only numbers and Letters", fieldName);
+                    error = true;
+                }
+                if (!(input.value.length > 3 && input.value.length < 9)) {
+                    addError(input, "Needs to be between 3-8 characters", fieldName);
+                    error = true;
+                }
+                if (fieldName == "password" != "password-confirm") {
+                    let exp = /[A-Za-z0-9]+$/;
+                    let result = exp.test(input.value);
+                    if (!result) {
+                        addError(input, "Only numbers and Letters", fieldName);
+                        error = true;
+                    }
+                    if (!(input.value.length > 3 && input.value.length < 9)) {
+                        addError(input, "Needs to be between 3-8 characters", fieldName);
+                        error = true;
+                    }
+
+                }
+            }
             data[fieldName] = input.value;
         }
-    });
+    })
     if (!error) {
         form.remove();
-        window.location = "/succes";
+        window.location = "/success";
     }
 }
 
@@ -59,5 +84,4 @@ function addError(input, msg, fieldName) {
     tempError.textContent = fieldName.toLowerCase() + " " + msg;
     tempError.classList.add("show-err");
     input.classList.add("border-red");
-    input.classList.add("animated", "rubberBand");
 }
